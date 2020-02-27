25ab63ff1dc449d416ae823c0f4687ca506f516a
Tom Hodgson <tom.hodgson@salecycle.com>
2020-02-27 11:14:02 +0000

    blue: refactor - compile check to make sure you cant add a card with value 15

--- ping-pong/src/index.spec.ts
+++ ping-pong/src/index.spec.ts
@@ -1,15 +1,54 @@
-enum picture {
-  King = 10,
-  Queen = 10,
-  Jack = 10,
-  Ace = 1
+enum Card {
+  King = "King",
+  Queen = "Queen",
+  Jack = "Jack",
+  Ten = "Ten",
+  Nine = "Nine",
+  Eight = "Eight",
+  Seven = "Seven",
+  Six = "Six",
+  Five = "Five",
+  Four = "Four",
+  Three = "Three",
+  Two = "Two",
+  Ace = "Ace"
 }
-type Card = number | picture;
+
+function value(card: Card): number {
+  switch (card) {
+    case Card.King:
+      return 10;
+    case Card.Queen:
+      return 10;
+    case Card.Jack:
+      return 10;
+    case Card.Ten:
+      return 10;
+    case Card.Nine:
+      return 9;
+    case Card.Eight:
+      return 8;
+    case Card.Seven:
+      return 7;
+    case Card.Six:
+      return 6;
+    case Card.Five:
+      return 5;
+    case Card.Four:
+      return 4;
+    case Card.Three:
+      return 3;
+    case Card.Two:
+      return 2;
+    case Card.Ace:
+      return 1;
+  }
+}
 
 function score(numbers: Card[]) {
-  const sum = numbers.reduce((acc, next) => acc + next, 0);
+  const sum = numbers.reduce((acc, next) => acc + value(next), 0);
 
-  return numbers.find(x => x === picture.Ace) && sum < 10
+  return numbers.find(x => x === Card.Ace) && sum < 10
     ? {
         lowAce: sum,
         highAce: sum + 9
@@ -18,21 +57,21 @@ function score(numbers: Card[]) {
 }
 
 test("score should return the total score of the cards", () => {
-  expect(score([2, 4])).toEqual(6);
-  expect(score([3, 4, 5])).toEqual(12);
+  expect(score([Card.Two, Card.Four])).toEqual(6);
+  expect(score([Card.Three, Card.Four, Card.Five])).toEqual(12);
 });
 
 test("score should count picture cards as 10", () => {
-  expect(score([3, picture.Queen, picture.King])).toEqual(23);
+  expect(score([Card.Three, Card.Queen, Card.King])).toEqual(23);
 });
 
 test("score should return a pair of scores when an ace is present", () => {
-  expect(score([3, picture.Ace])).toEqual({
+  expect(score([Card.Three, Card.Ace])).toEqual({
     lowAce: 4,
     highAce: 13
   });
 });
 
 test("score should return a single score when there's an ace but the rest of the cards sum to greater than 10", () => {
-  expect(score([3, picture.Queen, picture.Ace])).toEqual(14);
+  expect(score([Card.Three, Card.Queen, Card.Ace])).toEqual(14);
 });
