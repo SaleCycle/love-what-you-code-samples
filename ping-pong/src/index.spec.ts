a0e1641ceceadd531c4466980f5d395d60eb489d
Tom Hodgson <tom.hodgson@salecycle.com>
2020-02-27 10:34:42 +0000

    red: count three cards

--- ping-pong/src/index.spec.ts
+++ ping-pong/src/index.spec.ts
@@ -2,6 +2,7 @@ function score(numbers: number[]) {
   return 6;
 }
 
-test("a test", () => {
+test("score should return the total score of the cards", () => {
   expect(score([2, 4])).toEqual(6);
+  expect(score([3, 4, 5])).toEqual(12);
 });
