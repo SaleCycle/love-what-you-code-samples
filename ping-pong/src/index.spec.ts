a5ffea18bd0e8a8943270e6680f4f8fd1a958c6b
Tom Hodgson <tom.hodgson@salecycle.com>
2020-02-27 10:37:11 +0000

    red: picture cards

--- ping-pong/src/index.spec.ts
+++ ping-pong/src/index.spec.ts
@@ -6,3 +6,7 @@ test("score should return the total score of the cards", () => {
   expect(score([2, 4])).toEqual(6);
   expect(score([3, 4, 5])).toEqual(12);
 });
+
+test("score should count picture cards as 10", () => {
+  expect(score([3, Queen, King])).toEqual(23);
+});
