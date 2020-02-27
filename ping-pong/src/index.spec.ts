d21a3b663b544146151a8a4adf41f6c040db0813
Tom Hodgson <tom.hodgson@salecycle.com>
2020-02-27 10:53:52 +0000

    green: aces

--- ping-pong/src/index.spec.ts
+++ ping-pong/src/index.spec.ts
@@ -1,13 +1,19 @@
 enum picture {
   King = 10,
   Queen = 10,
-  Jack = 10
+  Jack = 10,
+  Ace = 1
 }
-
 type Card = number | picture;
 
 function score(numbers: Card[]) {
-  return numbers.reduce((acc, next) => acc + next, 0);
+  const sum = numbers.reduce((acc, next) => acc + next, 0);
+  return numbers.find(x => x === picture.Ace) === undefined
+    ? sum
+    : {
+        lowAce: sum,
+        highAce: sum + 9
+      };
 }
 
 test("score should return the total score of the cards", () => {
@@ -20,7 +26,7 @@ test("score should count picture cards as 10", () => {
 });
 
 test("score should return a pair of scores when an ace is present", () => {
-  expect(score([3, Ace])).toEqual({
+  expect(score([3, picture.Ace])).toEqual({
     lowAce: 4,
     highAce: 13
   });
