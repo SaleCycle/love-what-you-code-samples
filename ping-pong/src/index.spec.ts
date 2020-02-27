52c217468d6810d91afe90e0d085286476008722
Tom Hodgson <tom.hodgson@salecycle.com>
2020-02-27 10:42:14 +0000

    green: pictures count as 10

--- ping-pong/src/index.spec.ts
+++ ping-pong/src/index.spec.ts
@@ -1,4 +1,12 @@
-function score(numbers: number[]) {
+enum picture {
+  King = 10,
+  Queen = 10,
+  Jack = 10
+}
+
+type Card = number | picture;
+
+function score(numbers: Card[]) {
   return numbers.reduce((acc, next) => acc + next, 0);
 }
 
@@ -8,5 +16,5 @@ test("score should return the total score of the cards", () => {
 });
 
 test("score should count picture cards as 10", () => {
-  expect(score([3, Queen, King])).toEqual(23);
+  expect(score([3, picture.Queen, picture.King])).toEqual(23);
 });
